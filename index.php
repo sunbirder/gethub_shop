2a88e2173288e321d6dfd278d4970fd204eeb023
sunbirder <13489114740@139.com>
2013-12-28 13:13:19 +0800

    Revert "source"
    This reverts commit 022fa024b6be3083f8a0914d1fe226d83ce80908.

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-	date_default_timezone_set('PRC');
-	define('APP_NAME','APP');
-	define('APP_PATH','./APP/');
-	define('APP_DEBUG',TRUE);
-	// 自定义常量
-	
-	require './ThinkPHP/ThinkPHP.php';
-?>
