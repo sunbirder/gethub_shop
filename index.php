c0788f621211d7ef01cce53f197167d6c985790f
sunbirder <13489114740@139.com>
2013-12-28 13:16:52 +0800

    'index.php'

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+	date_default_timezone_set('PRC');
+	define('APP_NAME','APP');
+	define('APP_PATH','./APP/');
+	define('APP_DEBUG',TRUE);
+	// 自定义常量
+	
+	require './ThinkPHP/ThinkPHP.php';
+?>
